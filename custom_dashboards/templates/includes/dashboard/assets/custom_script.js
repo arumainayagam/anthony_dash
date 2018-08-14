5de0865b4a8e950108d0922845eb7bc841ec0a67
vicky <vignesh@crisoconsulting.in>
2018-08-14 16:57:56 +0530

    ad111

--- custom_dashboards/templates/includes/dashboard/assets/custom_script.js
+++ custom_dashboards/templates/includes/dashboard/assets/custom_script.js
@@ -2,9 +2,15 @@
 $(document).ready(function(){
 
 $("#dynachart").html("Sales Order");
-var ok;
-var labs = [];
-var dats = [];
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 frappe.call({
     method: "custom_dashboards.templates.pages.manufacturing_dash.get_chart_data",
@@ -13,60 +19,58 @@ frappe.call({
         doctype: "Sales Order",
     },
     callback: function (r) {
+        var ok;
+        var labs = [];
+        var dats = [];
+        var bgcol = [];
+        var borcol = [];
+
+
+               
         ok = r.message;
         console.log(ok)
         
+        console.log(labs)
+        console.log(dats)
+
+
         for (var i = 0; i < ok.length; i++) {
             labs.push(ok[i].status);
             dats.push(ok[i].valu);
+            bgcol.push(getRandomColor());
+            borcol.push(getRandomColor());
+
 
         }
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
 
-    }
-});
-console.log(labs)
-console.log(dats)
-
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-
-        labels: labs,
-        datasets: [{
-            label: '# of Votes',
-            data: dats,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+                labels: labs,
+                datasets: [{
+                    label: '# of Votes',
+                    data: dats,
+                    backgroundColor: bgcol,
+                    borderColor: borcol,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
                 }
-            }]
-        }
+            }
+        });
+
+
     }
 });
-
 
 $('.span8 #calendar_content #calendar').fullCalendar({
   events: [
@@ -96,46 +100,74 @@ $(".quick-btn").click(function () {
 
         $("#dynachart").html(b);
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+frappe.call({
+    method: "custom_dashboards.templates.pages.manufacturing_dash.get_chart_data",
+    async: false,
+    args: {
+        doctype: b,
     },
-    options: {
-        responsive: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+    callback: function (r) {
+        var ok;
+        var labs = [];
+        var dats = [];
+        var bgcol = [];
+        var borcol = [];
+
+
+               
+        ok = r.message;
+        console.log(ok)
+        
+        console.log(labs)
+        console.log(dats)
+
+
+        for (var i = 0; i < ok.length; i++) {
+            labs.push(ok[i].status);
+            dats.push(ok[i].valu);
+            bgcol.push(getRandomColor());
+            borcol.push(getRandomColor());
+
+
         }
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+
+                labels: labs,
+                datasets: [{
+                    label: '# of Votes',
+                    data: dats,
+                    backgroundColor: bgcol,
+                    borderColor: borcol,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+
     }
 });
-
 
     
 });
